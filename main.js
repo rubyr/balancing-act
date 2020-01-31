aa6e54c7c13aab8e45db70fdf5fedb10b69d345d
Ruby Rinken <liamrinken@gmail.com>
2020-01-31 16:27:44 -0700

    Add tab switching, redo some CSS

--- main.js
+++ main.js
@@ -1,15 +1,38 @@
+ var dashboard = document.querySelector("#dashboard");
+ var dashboardTab = document.querySelector("#dashboardTab");
+ var transactions = document.querySelector("#transactions");
+ var transactionsTab = document.querySelector("#transactionsTab");
+
+if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.getItem("hideBanner")) {
+        clearBanner();
+    }
+}
+
 function clearBanner() {
-    var banner = document.getElementById("banner")
-    banner.style.display = "none"
+    var banner = document.querySelector("#banner");
+    banner.classList.add("hidden");
     if (typeof(Storage) !== "undefined") {
         sessionStorage.setItem("hideBanner", "true");
     }
 }
 
-window.onload = function() {
-    if (typeof(Storage) !== "undefined") {
-        if (sessionStorage.getItem("hideBanner")) {
-            clearBanner()
-        }
-    }
+function openDashboardView() {
+    // select .selected tab, clear class
+    document.querySelector("li.selectedTab").classList.remove("selectedTab");
+    // select unhidden tabs, add hidden class
+    document.querySelector("main:not(.hidden)").classList.add("hidden");
+    // unhide dashboard tab
+    dashboard.classList.remove("hidden");
+    dashboardTab.classList.add("selectedTab");
+}
+
+function openTransactionsView() {
+    // select .selected tab, clear class
+    document.querySelector("li.selectedTab").classList.remove("selectedTab");
+    // select unhidden tabs, add hidden class
+    document.querySelector("main:not(.hidden)").classList.add("hidden");
+    // unhide dashboard tab
+    transactions.classList.remove("hidden");
+    transactionsTab.classList.add("selectedTab");
 }
